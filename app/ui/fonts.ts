a440ba1dea5f7f21ae5168716d37cbf7028bc6d1
winsanpang <winsanpang@hotmail.co.uk>
2025-01-02 17:36:11 +0000

    Start adding layout and basics

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Inter } from "next/font/google"
+import { Lobster } from "next/font/google"
+
+export const inter = Inter({ subsets: ["latin"] })
+export const lobster = Lobster({ weight: "400" })
