f251fda0316c99979cda12b794b7d38b11abf0ce
winsanpang <winsanpang@hotmail.co.uk>
2025-03-12 20:30:19 +0000

    Fix typo and remove redundant font awesome credit comment block

--- src/app/[locale]/ui/business-card.tsx
+++ src/app/[locale]/ui/business-card.tsx
@@ -1,19 +1,3 @@
-/* Copyright (c) 2025 - Win San Pang - https://fontawesome.com/icons/phone-alt / https://fontawesome.com/icons/envelope / https://fontawesome.com/icons/linkedin / https://fontawesome.com/icons/github / https://fontawesome.com/icons/twitter
-Original Phone, Envelope, LinkedIn GitHub, and Twitter FontAwesomeIcons made by Font Awesome, available for free on their website, MIT licensed with attribution.
-Font Awesome free licence: https://fontawesome.com/license/free
-
-The brand icons are a trademark of the respective owners. The use of this trademark does not indicate endorsement of the trademark holder by Font Awesome, nor vice versa.
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify,
-merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
 import { useTranslations } from "next-intl"
 import Link from "next/link"
 import Image from "next/image"
