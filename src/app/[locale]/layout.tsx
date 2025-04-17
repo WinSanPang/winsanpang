1a3a21f649d5b6ce56017b5d113ebf4370222db3
winsanpang <winsanpang@hotmail.co.uk>
2025-04-17 20:24:07 +0100

    Add footer

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,6 @@
 import { NextIntlClientProvider } from "next-intl"
 import { getMessages } from "next-intl/server"
+import { getTranslations } from "next-intl/server"
 
 import { ReactNode } from "react"
 import "@/app/[locale]/ui/global.css"
@@ -10,8 +11,15 @@ type Props = {
   children: ReactNode
 }
 
-export default async function RootLayout({ children }: Props) {
+export default async function RootLayout({ children, params }: Props) {
+  const { locale } = await params
+
   const messages = await getMessages()
+
+  const t = await getTranslations({
+    locale,
+    namespace: "footer",
+  })
 
   return (
     <html lang="en" className="scroll-smooth">
@@ -21,6 +29,11 @@ export default async function RootLayout({ children }: Props) {
             <Navigation />
             <div className="p-6 md:px-28 bg-gray-900">{children}</div>
           </main>{" "}
+          <footer className="bg-gray-900 text-white p-6 text-center">
+            <div className="border-b border-white border-dashed my-8 mx-28" />
+            <p>© {new Date().getFullYear()} Win San Pang</p>
+            <p>{t("made_with_love")}</p>
+          </footer>
         </NextIntlClientProvider>
       </body>
     </html>
